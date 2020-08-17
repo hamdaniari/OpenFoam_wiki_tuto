3f6e7d576acd91847c62ffccae4ffc3de6208c82
Ari Hamdani <aryham04@gmail.com>
2020-08-17 17:55:43 +0900

    Update basic8.C

--- day13/1basicCplus2/basic8.C
+++ day13/1basicCplus2/basic8.C
@@ -7,8 +7,10 @@ using namespace Foam;
 int main()
 {
 tensor t1(1,2,3,4,5,6,7,8,9);
+//cout << t1 << endl; cannot be compiled when trying to print t1
 cout << "t1[0]: " << t1[0] << endl;
 symmTensor st1(1,2,3,4,5,6);
+//cout << st1 << endl;
 cout << "st1[5]: " << st1[5] << endl;
 return 0;
 }
